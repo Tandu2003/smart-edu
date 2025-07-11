0496a1633143d1856f4fa93161e29028862577de
Tấn Dự <tandu542003@gmail.com>
2025-07-11 11:09:14 +0700

    chore: initialize Smart Edu project with standard folder structure, ESLint, Prettier, Vite, React, TypeScript, and essential starter files (README, index.html, public, src). Ready for modern frontend development.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Toaster } from 'sonner';
+
+function App() {
+  return (
+    <>
+      <Toaster position="top-right" richColors closeButton duration={3000} />
+      {/* TODO: Add your components here */}
+    </>
+  );
+}
+
+export default App;
